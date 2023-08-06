f8daf591db16761a0bfb1e4d0bdd817bb631d419
leliasgomer <leliasgomer@gmail.com>
2023-08-05 20:02:27 -0500

    fix: :bug: master delete comments and change title and description

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,7 @@ import Content from './components/Content'
 import { globalStore } from './state/store'
 
 export default function Home () {
-  // const [prompt, setPrompt] = useState('')
   const { text } = globalStore()
-
-  // async function handleSubmit (e: FormEvent<HTMLElement>) {
-  //   e.preventDefault()
-  //   const data = await axios.post('/api/hello', { prompt })
-  //   const resp = await data.data
-  //   console.log(resp)
-  // }
   return (
     <main className="flex h-screen flex-col items-center justify-between md:p-16 p-4 bg-[#343541]">
       {!text && <Title />}
